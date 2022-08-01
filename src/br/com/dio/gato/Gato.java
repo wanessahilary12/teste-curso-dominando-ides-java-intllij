408f7ea9e1950e3ba87002046ee8aef8842875ca
Wanessa HIlary <wanessahillary4@gmail.com>
2022-08-01 15:57:42 -0300

    first commit

---
+++ src/br/com/dio/gato/Gato.java
@@ -0,0 +1,4 @@
+package br.com.dio.gato;
+
+public interface Gato {
+}
---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+
+
+       /* int a = 5;
+        int b = 2;
+        System.out.println("hello world!" + (a + b));*/
+    }
+}
